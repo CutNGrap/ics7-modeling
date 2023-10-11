88d978a55d9dbda8ab9975bbc63287fa03080071
grapess <grappess@mail.ru>
2023-10-12 00:01:09 +0300

    migrate

--- lab_01/src/code.m
+++ lab_01/src/code.m
@@ -1,31 +1,26 @@
 figure('Name', 'Graph 1')
 h = 0.01;
-x = 1:0.01:7;
 a = 2;
 b = 5;
-a1 = 2;
-b1 = 7;
-a2 = 1;
-b2 = 3;
-x = a:h:b;
-x1 = 1:h:7;
-x2 = 1:h:7;
+a1 = 1;
+b1 = 6;
+a2 = 3;
+b2 = 5;
 
-y = (unifpdf(x,a,b)).*((a <= x) && (x <= b)) + (0)*(x > b) + (0)*(x<a);
-plot(x,y, 'LineWidth',2, 'DisplayName','a=2, b=5'), grid;
+
+x = 0:h:7;
+
+y = unifpdf(x,a1,b1);
+plot(x,y, 'LineWidth',2, 'DisplayName','a=1, b=6'), grid;
 xlim([0, 7]);
+ylim([-0.2, 0.8]);
 legend;
 
-%%
 figure('Name', 'Graph 2')
- plot(x,unifcdf(x,a,b), 'DisplayName','a=2, b=5'), grid;
- hold on;
- plot(x1, unifcdf(x1, a1, b1), 'DisplayName','a=2, b=7');
- hold on;
- plot(x2, unifcdf(x2, a2, b2), 'DisplayName','a=1, b=3');
+ plot(x,unifcdf(x,a1,b1), 'DisplayName','a=1, b=6'), grid;
  legend;
- xlim([0.5, 9.5]);
- ylim([-0.1, 1.1])
+xlim([0, 7]);
+ylim([-0.2, 1.2]);
  %%
  
 MX = 0;
@@ -41,9 +36,17 @@ MX3 = -2;
 DX3 = 0.5;
 figure('Name', 'Graph 3')
 xx = -5:0.01:5;
- plot(xx,normpdf(xx,MX,DX)), grid;
+ plot(xx,normpdf(xx,MX2,DX2)), grid;
  
- %%
+ xlim([-5, 5]);
+ ylim([0, 1]);
+ 
+ 
+ 
 figure ('Name', 'Graph 4')
+
+plot(xx,normcdf(xx,MX2,DX2)), grid;
+xlim([-5, 5]);
+ ylim([0, 1]);
 
 
